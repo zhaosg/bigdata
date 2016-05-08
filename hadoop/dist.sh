a4fa856b93c338f193d1dd090c33abe884b3b23b
zhaosg <zhaosg89@163.com>
2016-05-08 13:09:29 +0800

    文档完善

--- hadoop/dist.sh
+++ hadoop/dist.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 hosts="nn01 nn02 nn03 nn04 dn01 dn02 dn03"
-for i in $hosts
+user=zhaosg
+password=mima0704
+hadoop_conf=/opt/app/hadoop-2.6.2/etc
+filepath=$(cd "$(dirname "$0")"; pwd)
+local_files=${filepath}/conf1/etc/hadoop
+
+for host in $hosts
 do
-    scp ./conf1/* zhaosg@${i}:/opt/app/hadoop-2.6.2/etc/hadoop
+    expect -c "
+     set timeout 80
+     spawn scp -r ${local_files} ${user}@${host}:${hadoop_conf}
+     expect {
+         yes/no { send \"yes\r\"; exp_continue }
+         *password:* {send -- ${password}\r}
+     }
+     expect eof
+    "
 done
