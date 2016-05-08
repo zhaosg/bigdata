369274a5bd7b73129685b7b6c3e429db83cf2c3d
zhaosg <zhaosg89@163.com>
2016-05-08 16:32:42 +0800

    文档完善

--- hadoop/format.sh
+++ hadoop/format.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 filepath=$(cd "$(dirname "$0")"; pwd)
 source ${filepath}/env.sh
-${filepath}/start-jn.sh
+${filepath}/start-qjm.sh
 ${filepath}/format-zk.sh
 ${filepath}/format-dfs.sh
