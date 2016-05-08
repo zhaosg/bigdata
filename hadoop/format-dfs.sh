7a1439ac72178f24d205f0110c1784ab28d06e0b
zhaosg <zhaosg89@163.com>
2016-05-08 16:34:26 +0800

    文档完善

--- hadoop/format-dfs.sh
+++ hadoop/format-dfs.sh
@@ -2,7 +2,6 @@
 filepath=$(cd "$(dirname "$0")"; pwd)
 source ${filepath}/env.sh
 local_files=${filepath}/etc/hadoop
-${filepath}/start-jn.sh
 expect -c "
  set timeout 80
  spawn ssh -q ${user}@${namenode}  ${hadoop_home}/bin/hadoop namenode -format -force -clusterId cluster1
