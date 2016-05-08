81e42cabeff6756e59809c6fc4a8677f8f466422
zhaosg <zhaosg89@163.com>
2016-05-08 20:38:52 +0800

    文档完善

--- hadoop/nn-status.sh
+++ hadoop/nn-status.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 filepath=$(cd "$(dirname "$0")"; pwd)
 source ${filepath}/env.sh
+local_files=${filepath}/etc/hadoop
 for nn in $namenodes
 do
     expect -c "
      set timeout 80
-     spawn ssh -q ${user}@${nn}  ${hadoop_home}/sbin/hadoop-daemon.sh start namenode
+     spawn ssh -q ${user}@${nn}  ${hadoop_home}/bin/hdfs  haadmin -getServiceState  ${nn}
      expect {
          yes/no { send \"yes\r\"; exp_continue }
          *password:* {send -- ${password}\r}
