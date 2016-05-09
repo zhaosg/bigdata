c694f2af4483f43ca7ee5ed6735dab196cd5067a
zhaosg <zhaosg89@163.com>
2016-05-09 20:31:34 +0800

    文档完善

--- hadoop/env.sh
+++ hadoop/env.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-hosts="nn01 nn02 nn03 nn04 dn01 dn02 dn03"
+hosts="nn01 nn02 dn01 dn02 dn03"
 namenode=nn01
 namenodes="nn01 nn02"
 datanodes="dn01 dn02 dn03"
--- hadoop/format.sh
+++ hadoop/format.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 filepath=$(cd "$(dirname "$0")"; pwd)
 source ${filepath}/env.sh
-${filepath}/format-zk.sh
 ${filepath}/start-qjm.sh
+${filepath}/format-zk.sh
 ${filepath}/format-dfs.sh
 ${filepath}/stop-qjm.sh
